71b9785de0bae5daa376d098db0d93406740dc25
Rahul Dutta <rdtech2002@gmail.com>
2023-08-09 18:07:58 +0530

    .

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./projects/*.html"],
+  theme: {
+    fontFamily: {
+      'Yatra One': ['Yatra One', 'cursive']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
